7abe51a5b23f8207736f09f579a2956f24d71ce2
justin-hong22 <hong.justin6@gmail.com>
2024-11-05 00:36:36 +0900

    implemented chat history box

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,6 @@
+let firstQuestionBool = true;
+let currentQuestion = "";
+
 function answerQuestion()
 {
     const question = document.getElementById('question_box').value;
@@ -7,6 +10,25 @@ function answerQuestion()
         body: JSON.stringify({ question: question })
     })
     .then(response => response.json())
-    .then(data => {document.getElementById('answer_box').value = data.response; })
-    .catch(error => console.error("ERROR - ", error));
+    .then(data => {
+        document.getElementById('answer_box').value = data.response;
+
+        const answer = document.getElementById('answer_box').value;
+        addHistory(question, answer);
+    })
+    .catch(error => console.error("ERROR - ", error));   
+}
+
+function addHistory(question, answer)
+{
+    if(!firstQuestionBool)
+    {
+        const historyBoxContents = document.getElementById('history_box').value;
+        const history = historyBoxContents == "" ? currentQuestion : historyBoxContents + currentQuestion;
+        document.getElementById('history_box').value = "";
+        document.getElementById('history_box').value = history;
+    }
+
+    currentQuestion = "Question : " + question + '\n\n' + "Answer : " + answer + '\n\n' + "=".repeat(150) + '\n\n';
+    firstQuestionBool = false;
 }
