47b86ffd6e67c6bc191aed0b3f678dbbea88fdb1
justin-hong22 <hong.justin6@gmail.com>
2024-11-16 01:26:31 +0900

    added japanese and chinese translations

--- static/js/index.js
+++ static/js/index.js
@@ -35,3 +35,33 @@ function addHistory(question, answer)
 function clearBox(box) {
     box.value = "";
 }
+
+function changeLanguage(language)
+{
+    switch(language)
+    {
+        case "en":
+            document.getElementById("title").innerHTML = "Business Negotiation Chatbot";
+            document.getElementById("input_title").innerHTML = "Question Input";
+            document.getElementById("question_btn").innerHTML = "Generate Answer";
+            document.getElementById("output_title").innerHTML = "Answer Output";
+            document.getElementById("history_title").innerHTML = "Chat History";
+            break;
+
+        case "jp":
+            document.getElementById("title").innerHTML = "ビジネス交渉　チャットボット";
+            document.getElementById("input_title").innerHTML = "質問入力";
+            document.getElementById("question_btn").innerHTML = "質問を聞く";
+            document.getElementById("output_title").innerHTML = "答え出力";
+            document.getElementById("history_title").innerHTML = "チャット歴史";
+            break;
+
+        case "zh":
+            document.getElementById("title").innerHTML = "商務談判網路聊天系統";
+            document.getElementById("input_title").innerHTML = "問題輸入";
+            document.getElementById("question_btn").innerHTML = "生成答案";
+            document.getElementById("output_title").innerHTML = "答案輸出";
+            document.getElementById("history_title").innerHTML = "聊天記錄";
+            break;
+    }
+}
